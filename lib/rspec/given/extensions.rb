b115f5da110568a44f3f6775468b47d0542a5daf
Jim Weirich <jim.weirich@gmail.com>
2010-11-27 15:24:21 -0500

    Added Given! and remoted when_result vars.

--- lib/rspec/given/extensions.rb
+++ lib/rspec/given/extensions.rb
@@ -17,13 +17,20 @@ module RSpec
           if args.first.is_a?(Symbol)
             let(args.first, &block)
           else
-            let!(:given_result,&block)
+            before(&block)
           end
         end
 
+        def Given!(var, &block)
+          let!(var, &block)
+        end
+
         def When(*args, &block)
-          sym = args.first.is_a?(Symbol) ? args.first : :when_result
-          let!(sym, &block)
+          if args.first.is_a?(Symbol)
+            let!(args.first, &block)
+          else
+            before(&block)
+          end
         end
       end
     end
