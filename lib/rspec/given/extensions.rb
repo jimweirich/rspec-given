e8b36ede4c3e7adffa49b74374694287458bf4f9
David Chelimsky <dchelimsky@gmail.com>
2010-11-28 14:10:11 -0600

    use the RSpec.configure API instead of monkey patching

--- lib/rspec/given/extensions.rb
+++ lib/rspec/given/extensions.rb
@@ -1,38 +1,31 @@
-require 'rspec/core/let'
-
 module RSpec
-  module Core
-    class ExampleGroup
-      alias_example_to :Then
-    end
-  end
-end
-
-module RSpec
-  module Core
-    module Let
-      module ClassMethods
-
-        def Given(*args,&block)
-          if args.first.is_a?(Symbol)
-            let(args.first, &block)
-          else
-            before(&block)
-          end
+  module Given
+    module Extensions
+      def Given(*args,&block)
+        if args.first.is_a?(Symbol)
+          let(args.first, &block)
+        else
+          before(&block)
         end
+      end
 
-        def Given!(var, &block)
-          let!(var, &block)
-        end
+      def Given!(var, &block)
+        let!(var, &block)
+      end
 
-        def When(*args, &block)
-          if args.first.is_a?(Symbol)
-            let!(args.first, &block)
-          else
-            before(&block)
-          end
+      def When(*args, &block)
+        if args.first.is_a?(Symbol)
+          let!(args.first, &block)
+        else
+          before(&block)
         end
       end
     end
   end
 end
+
+RSpec.configure do |c| 
+  c.alias_example_to :Then
+  c.extend(RSpec::Given::Extensions)
+end
+
