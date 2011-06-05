19a9a2f5f5b1aec9491e232b53f3a2b407cdfb83
Jim Weirich <jim.weirich@gmail.com>
2011-06-04 22:57:51 -0400

    Updated copyright dates and bumped to version 1.4.0.

--- lib/rspec/given/version.rb
+++ lib/rspec/given/version.rb
@@ -1,9 +1,10 @@
 module RSpec
   module Given
-    VERSION_MAJOR = 1
-    VERSION_MINOR = 3
-    VERSION_BUILD = 1
-    VERSION_NUMBERS = [VERSION_MAJOR, VERSION_MINOR, VERSION_BUILD]
+    VERSION_NUMBERS = [
+      VERSION_MAJOR = 1,
+      VERSION_MINOR = 4,
+      VERSION_BUILD = 0,
+    ]
     VERSION = VERSION_NUMBERS.join(".")
   end
 end
