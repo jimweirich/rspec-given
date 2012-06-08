3e4a40d7fbeee3d59e55d8e839d90fc0e8ca903b
Jim Weirich <jim.weirich@gmail.com>
2012-06-08 13:24:38 -0400

    Bump to version 1.5.0

--- lib/rspec/given/version.rb
+++ lib/rspec/given/version.rb
@@ -2,8 +2,8 @@ module RSpec
   module Given
     VERSION_NUMBERS = [
       VERSION_MAJOR = 1,
-      VERSION_MINOR = 4,
-      VERSION_BUILD = 2,
+      VERSION_MINOR = 5,
+      VERSION_BUILD = 0,
     ]
     VERSION = VERSION_NUMBERS.join(".")
   end
