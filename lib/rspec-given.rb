23c093c3c428c3053e89e9220c797c4530b5df1a
Jim Weirich <jim.weirich@gmail.com>
2012-06-08 13:17:14 -0400

    Add explanatory comment to bundler require file.

--- lib/rspec-given.rb
+++ lib/rspec-given.rb
@@ -1,1 +1,9 @@
+# This file file is to make bundler happy when auto-requiring files
+# based on gem name.  If you are manually requiring rspec/given,
+# please use the canonical require file, ie.
+#
+#     require 'rspec/given'
+#
+# Thanks.
+
 require 'rspec/given'
