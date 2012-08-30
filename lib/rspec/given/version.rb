2f63ab435e4a95e756acbf27f4e48109164ea9a7
Jim Weirich <jim.weirich@gmail.com>
2012-08-30 03:05:29 -0600

    Bump to version 1.6.0

--- lib/rspec/given/version.rb
+++ lib/rspec/given/version.rb
@@ -4,8 +4,6 @@ module RSpec
       VERSION_MAJOR = 1,
       VERSION_MINOR = 6,
       VERSION_BUILD = 0,
-      VERSION_BETA = "beta",
-      VERSION_BETANUM = "1",
     ]
     VERSION = VERSION_NUMBERS.join(".")
   end
