8b63f3a9890a95bc1d24e1c36a005fd183e95f64
Jim Weirich <jim.weirich@gmail.com>
2012-09-05 22:01:52 -0400

    Add test for focused line spec running

---
+++ examples/integration/focused_line_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "Focused Line" do
+  it "runs only a single test" do
+    output = `rspec examples/other/line_example.rb:7`
+    output.should_not =~ /FIRST/
+  end
+end
---
+++ examples/other/line_example.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Line Spec" do
+
+  Then { puts "FIRST" }
+
+  Then { puts "SECOND" }
+
+end
