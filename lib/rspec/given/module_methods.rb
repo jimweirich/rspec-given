2de57aec8c37399fa24f1ff03e9d58a9363b3a19
Jim Weirich <jim.weirich@gmail.com>
2012-09-13 11:38:29 -0400

    document source_cache_disabled configuration

--- lib/rspec/given/module_methods.rb
+++ lib/rspec/given/module_methods.rb
@@ -1,16 +1,16 @@
 module RSpec
   module Given
-    def self.html_format_disabled
-      @_rg_html_format_disabled
+    def self.source_caching_disabled
+      @_rg_source_caching_disabled
     end
 
-    def self.html_format_disabled=(value)
-      @_rg_html_format_disabled = value
+    def self.source_caching_disabled=(value)
+      @_rg_source_caching_disabled = value
     end
 
     def self.detect_formatters(c)
       format_active = c.formatters.any? { |f| f.class.name !~ /ProgressFormatter/ }
-      RSpec::Given.html_format_disabled = ! format_active
+      RSpec::Given.source_caching_disabled = ! format_active
     end
   end
 end
