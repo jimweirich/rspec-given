c73753c7033c6f4c3dedac750290f4eae484841a
Jim Weirich <jim.weirich@gmail.com>
2012-09-14 22:26:12 -0400

    flex methods in framework and describe blocks.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,11 @@ require 'rspec/given'
 RSpec.configure do |config|
   config.mock_with :flexmock
   puts "DBG: config=#{config.inspect}"
+  config.include FlexMock::MockContainer
   config.include_or_extend_modules.each do |ccc| puts "DBG: ccc=#{ccc.inspect}" end
+  puts "DBG: RSpec::Core::MockFrameworkAdapter.instance_methods=#{RSpec::Core::MockFrameworkAdapter.instance_methods.grep(/flex/).inspect}"
+end
+
+describe "SAMPLE" do
+  puts "DBG: in SAMPLE instance_methods=#{instance_methods.grep(/flex/).inspect}"
 end
