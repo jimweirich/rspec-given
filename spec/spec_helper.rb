235d44afaa16be9d459283af4197d2e240f17600
Jim Weirich <jim.weirich@gmail.com>
2012-09-14 22:15:53 -0400

    what modules are loaded?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,6 @@ require 'rspec/given'
 
 RSpec.configure do |config|
   config.mock_with :flexmock
+  puts "DBG: config=#{config.inspect}"
+  config.include_or_extend_modules.each do |ccc| puts "DBG: ccc=#{ccc.inspect}" end
 end
