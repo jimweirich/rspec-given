420e3d1b0903a683845205cc052e131e5404d2f1
Jim Weirich <jim.weirich@gmail.com>
2012-11-15 14:28:59 -0500

    Bump to version 2.2
    Compatibility changes for RSpec-2.12

--- lib/rspec/given/version.rb
+++ lib/rspec/given/version.rb
@@ -2,7 +2,7 @@ module RSpec
   module Given
     VERSION_NUMBERS = [
       VERSION_MAJOR = 2,
-      VERSION_MINOR = 1,
+      VERSION_MINOR = 2,
       VERSION_BUILD = 0,
     ]
     VERSION = VERSION_NUMBERS.join(".")
