691f272c6753da63ded10d48336f0dfe8b42363c
Jim Weirich <jim.weirich@gmail.com>
2012-12-03 14:46:12 +1000

    Moved core functionality requires to a core.rb file.

--- lib/rspec/given.rb
+++ lib/rspec/given.rb
@@ -12,12 +12,7 @@ end
 if RSpec::Given.using_old_rspec?
   require 'rspec/given/rspec1_given'
 else
-  require 'rspec/given/version'
-  require 'rspec/given/module_methods'
-  require 'rspec/given/file_cache'
-  require 'rspec/given/line_extractor'
-  require 'rspec/given/extensions'
-  require 'rspec/given/configure'
-  require 'rspec/given/failure'
+  require 'rspec/given/core'
   require 'rspec/given/have_failed'
+  require 'rspec/given/configure'
 end
---
+++ lib/rspec/given/core.rb
@@ -0,0 +1,9 @@
+
+# Require all the core features
+
+require 'rspec/given/version'
+require 'rspec/given/module_methods'
+require 'rspec/given/file_cache'
+require 'rspec/given/line_extractor'
+require 'rspec/given/extensions'
+require 'rspec/given/failure'
