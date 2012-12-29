674cb54e49bc755eea21b6108180a09662e0d964
Jim Weirich <jim.weirich@gmail.com>
2012-12-28 21:52:38 -0500

    Add additional failing examples.

--- examples/failing/natural_failing_spec.rb
+++ examples/failing/natural_failing_spec.rb
@@ -5,6 +5,7 @@ describe "Natural Assertions" do
   Given(:foo) { 1 }
   Given(:expected) { 2 }
   Given(:ary) { [1] }
+  Given(:empty) { [] }
   Given(:null) { nil }
   Then { foo+foo+2*foo == expected }
   Then { nil == "HI" && true && :symbol  && 1}
@@ -16,4 +17,19 @@ describe "Natural Assertions" do
   Then { true }
   And  { !null.nil? }
   Then { fail "OUCH" }
+  Then { ! empty.empty? }
+  Then {
+    (puts "Ha ha world", ! true)
+  }
+
+  context "Incorrect non-idempotent conditions" do
+    Given(:ary) { [1, 2, 3] }
+    Then { ary.delete(1) == nil }
+  end
+
+  context "Correct idempotent conditions" do
+    Given(:ary) { [1, 2, 3] }
+    When(:result) { ary.delete(1) }
+    Then { result == nil }
+  end
 end
