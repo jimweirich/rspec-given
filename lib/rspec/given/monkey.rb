d06b36a4eac4b1b58fade6f733a886dc808dc170
Jim Weirich <jim.weirich@gmail.com>
2013-01-04 22:35:55 -0500

    Prevent double inclusion of the RSpec monkey patch.

--- lib/rspec/given/monkey.rb
+++ lib/rspec/given/monkey.rb
@@ -2,35 +2,39 @@ require 'rspec'
 
 # Monkey patch RSpec to detect matchers used in expectations.
 
-if defined?(RSpec::Expectations::PositiveExpectationHandler) &&
-    defined?(RSpec::Expectations::NegativeExpectationHandler)
+unless defined?(RSpec::Given::MONKEY)
 
-  RSpec::Given::MONKEY = true
+  if defined?(RSpec::Expectations::PositiveExpectationHandler) &&
+      defined?(RSpec::Expectations::NegativeExpectationHandler)
 
-  module RSpec
-    module Expectations
-      class PositiveExpectationHandler
-        class << self
-          alias _rg_rspec_original_handle_matcher handle_matcher
-          def handle_matcher(actual, matcher, message=nil, &block)
-            RSpec::Given.matcher_called = true
-            _rg_rspec_original_handle_matcher(actual, matcher, message, &block)
+    RSpec::Given::MONKEY = true
+
+    module RSpec
+      module Expectations
+        class PositiveExpectationHandler
+          class << self
+            alias _rg_rspec_original_handle_matcher handle_matcher
+            def handle_matcher(actual, matcher, message=nil, &block)
+              RSpec::Given.matcher_called = true
+              _rg_rspec_original_handle_matcher(actual, matcher, message, &block)
+            end
           end
         end
-      end
 
-      class NegativeExpectationHandler
-        class << self
-          alias _rg_rspec_original_handle_matcher handle_matcher
-          def handle_matcher(actual, matcher, message=nil, &block)
-            RSpec::Given.matcher_called = true
-            _rg_rspec_original_handle_matcher(actual, matcher, message, &block)
+        class NegativeExpectationHandler
+          class << self
+            alias _rg_rspec_original_handle_matcher handle_matcher
+            def handle_matcher(actual, matcher, message=nil, &block)
+              RSpec::Given.matcher_called = true
+              _rg_rspec_original_handle_matcher(actual, matcher, message, &block)
+            end
           end
         end
       end
     end
+
+  else
+    RSpec::Given::MONKEY = false
   end
 
-else
-  RSpec::Given::MONKEY = false
 end
