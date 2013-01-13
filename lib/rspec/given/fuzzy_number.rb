e6c872da76da311d0d4abc8793c7c5d59af9a0c7
Jim Weirich <jim.weirich@gmail.com>
2013-01-13 11:59:13 -0500

    Improved FuzzyNumber initialization options.

--- lib/rspec/given/fuzzy_number.rb
+++ lib/rspec/given/fuzzy_number.rb
@@ -4,11 +4,17 @@ module RSpec
     module Fuzzy
       class FuzzyNumber         # :nodoc:
 
-        EPSILON = 10 * Float::EPSILON
+        DEFAULT_EPSILON = 10 * Float::EPSILON
 
-        def initialize(number, delta=nil)
+        def initialize(number, options=nil)
           @number = number
-          @delta = delta || (number * EPSILON)
+          case options
+          when Numeric
+            @delta = options
+          when Hash
+            @delta = delta_from_options(options, number)
+          end
+          @delta ||= (number * DEFAULT_EPSILON)
         end
 
         def ==(other)
@@ -18,6 +24,44 @@ module RSpec
         def to_s
           "<Approximately #{@number} +/- #{@delta}>"
         end
+
+        private
+
+        OPTIONS = {
+          epsilon: ->(neps, number)    { number * (neps * Float::EPSILON) },
+          percent: ->(percent, number) { number * (percent / 100.0) },
+          delta:   ->(delta, number)   { delta },
+        }
+
+        def delta_from_options(options, number)
+          validate_hash_options(options)
+          key = options.keys.first
+          OPTIONS[key].(options[key], number)
+        end
+
+        def validate_hash_options(options)
+          validate_only_one_option(options)
+          validate_known_options(options)
+        end
+
+        def validate_only_one_option(options)
+          if options.size < 1
+            fail ArgumentError, "No options given"
+          end
+          if options.size > 1
+            fail ArgumentError, "Too many options: '#{options.keys.join(', ')}'"
+          end
+        end
+
+        VALID_KEYS = OPTIONS.keys
+
+        def validate_known_options(options)
+          options.keys.each do |k|
+            if ! VALID_KEYS.include?(k)
+              fail ArgumentError, "Invalid option: '#{k}'"
+            end
+          end
+        end
       end
 
       # Create an approximate number that is approximately equal to
@@ -25,8 +69,8 @@ module RSpec
       # explicit delta is given, then the default delta that is about
       # 10X the size of the smallest possible change in the given
       # number will be used.
-      def about(number, delta=nil)
-        FuzzyNumber.new(number, delta)
+      def about(*args)
+        FuzzyNumber.new(*args)
       end
     end
   end
--- spec/lib/rspec/given/fuzzy_number_spec.rb
+++ spec/lib/rspec/given/fuzzy_number_spec.rb
@@ -4,56 +4,110 @@ describe RSpec::Given::Fuzzy::FuzzyNumber do
   use_natural_assertions
   include RSpec::Given::Fuzzy
 
-  Given(:delta) { 0.0001 }
-  Given(:number) { about(10, delta) }
 
-  context "when exact match" do
-    Then { 10 == number }
-    Then { number == 10 }
-  end
+  describe "fixed deltas" do
+    context "when created with non-hash delta" do
+      Given(:delta) { 0.0001 }
+      Given(:number) { about(10, delta) }
 
-  context "when (barely) in range" do
-    Then { (10 + 0.0001) == number }
-    Then { (10 - 0.0001) == number }
-  end
+      Then { 10 == number }
+      Then { number == 10 }
 
-  context "when out of range" do
-    Then { (10 + 0.000100001) != number }
-    Then { (10 - 0.000100001) != number }
-  end
+      Then { (10 + 0.0001) == number }
+      Then { (10 - 0.0001) == number }
 
-  describe "default delta" do
-    N = 10
-    HI_IN_RANGE = 1 + N*Float::EPSILON
-    LO_IN_RANGE = 1 - N*Float::EPSILON
-    HI_OUT_OF_RANGE = 1 + (N+1)*Float::EPSILON
-    LO_OUT_OF_RANGE = 1 - (N+1)*Float::EPSILON
-
-    Given(:number) { about(exact_number) }
-
-    Invariant { exact_number*HI_IN_RANGE == number }
-    Invariant { exact_number*LO_IN_RANGE == number }
-
-    Invariant { exact_number*HI_OUT_OF_RANGE != number }
-    Invariant { exact_number*LO_OUT_OF_RANGE != number }
-
-    context "when 1" do
-      Given(:exact_number) { 1 }
-      Then { exact_number == number }
+      Then { (10 + 0.000100001) != number }
+      Then { (10 - 0.000100001) != number }
     end
 
-    context "when rather large" do
-      Given(:exact_number) { 1_000_000 }
+    context "when created with explicit delta" do
+      Given(:exact_number) { 10 }
+      Given(:number) { about(exact_number, delta: 0.001) }
+
       Then { exact_number == number }
+
+      Then { (exact_number + 0.001) == number }
+      Then { (exact_number - 0.001) == number }
+
+      Then { (exact_number + 0.001001) != number }
+      Then { (exact_number - 0.001001) != number }
+    end
+  end
+
+  describe "percentage deltas" do
+    Given(:exact_number) { 1 }
+    Given(:number) { about(exact_number, percent: 25) }
+
+    Then { exact_number == number }
+
+    Then { (exact_number + 0.25) == number }
+    Then { (exact_number - 0.25) == number }
+
+    Then { (exact_number + 0.25001) != number }
+    Then { (exact_number - 0.25001) != number }
+  end
+
+  describe "small epsilon deltas" do
+    Given(:neps) { 10 }
+    Given(:hi_in_range) { 1 + neps*Float::EPSILON }
+    Given(:lo_in_range) { 1 - neps*Float::EPSILON }
+    Given(:hi_out_of_range) { 1 + (neps+1)*Float::EPSILON }
+    Given(:lo_out_of_range) { 1 - (neps+1)*Float::EPSILON }
+
+    Invariant { exact_number*hi_in_range == number }
+    Invariant { exact_number*lo_in_range == number }
+
+    Invariant { exact_number*hi_out_of_range != number }
+    Invariant { exact_number*lo_out_of_range != number }
+
+    context "when created with default delta" do
+      Given(:number) { about(exact_number) }
+
+      context "when 1" do
+        Given(:exact_number) { 1 }
+        Then { exact_number == number }
+      end
+
+      context "when rather large" do
+        Given(:exact_number) { 1_000_000 }
+        Then { exact_number == number }
+      end
+
+      context "when rather small" do
+        Given(:exact_number) { 0.000_001 }
+        Then { exact_number == number }
+      end
     end
 
-    context "when rather small" do
-      Given(:exact_number) { 0.000_001 }
+    context "when created with small epsilon" do
+      Given(:neps) { 100 }
+      Given(:exact_number) { 10 }
+      Given(:number) { about(exact_number, epsilon: neps) }
       Then { exact_number == number }
     end
   end
 
   describe "#to_s" do
+    Given(:number) { about(10, delta: 0.0001) }
     Then { number.to_s == "<Approximately 10 +/- 0.0001>" }
+  end
+
+  describe "invalid options" do
+    context "with an illegal option" do
+      When(:result) { about(10, junk: 1) }
+      Then { result == have_failed(ArgumentError, /invalid.*junk/i) }
+    end
+
+    context "with too many options" do
+      When(:result) { about(10, epsilon: 1, delta: 10) }
+      Then { result == have_failed(ArgumentError, /too many/i) }
+      And  { result == have_failed(ArgumentError, /epsilon/i) }
+      And  { result == have_failed(ArgumentError, /delta/i) }
+    end
+
+    context "with no options" do
+      When(:result) { about(10, {}) }
+      Then { result == have_failed(ArgumentError, /no options/i) }
+    end
   end
 end
