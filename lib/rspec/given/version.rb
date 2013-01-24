8a59fac34fc4f52bf16394e3c39d774ee719a05d
Jim Weirich <jim.weirich@gmail.com>
2013-01-24 17:57:18 -0500

    Bump to version 2.3.1.beta.1.

--- lib/rspec/given/version.rb
+++ lib/rspec/given/version.rb
@@ -3,7 +3,8 @@ module RSpec
     VERSION_NUMBERS = [
       VERSION_MAJOR = 2,
       VERSION_MINOR = 3,
-      VERSION_BUILD = 0,
+      VERSION_BUILD = 1,
+      'beta', 1
     ]
     VERSION = VERSION_NUMBERS.join(".")
   end
