95102760c8926d90d9e4fb62e3edf1babe8ea5b6
Jim Weirich <jim.weirich@gmail.com>
2013-03-20 16:46:08 -0300

    Move FauxThen into the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,52 @@ end
 RSpec.configure do |c|
   c.extend(ContextMethods)
 end
+
+
+# The Faux module defines a FauxThen that is used to setup an
+# environment identical to the real Then blocks in order to setup a
+# realistic NaturalAssertion object that can be used for making
+# assertions.
+#
+# Typical Usage:
+#
+#     context "with something" do
+#       Given(:a) { 1 }
+#       FauxThen { a + 2 }
+#       Then { result_block == 3 }
+#       Then { na.evaluate("a") == 1 }
+#     end
+#
+# The FauxThen sets up two special values:
+#
+# * result_block -- is the result of evaluating the FauxThen block
+# * na -- is a the natural assertion object whose context is the
+#         FauxThen block.
+#
+module Faux
+  module CX
+    def FauxThen(&block)
+      @block = block
+    end
+    def the_block
+      @block
+    end
+  end
+
+  module IX
+    def block_result
+      instance_eval(&self.class.the_block)
+    end
+
+    def na
+      block = self.class.the_block
+      RSpec::Given::NaturalAssertion.new("FauxThen", block, self, self.class._rgc_lines)
+    end
+  end
+end
+
+# Extend RSpec with our Faux Then blocks
+RSpec.configure do |c|
+  c.extend(Faux::CX)
+  c.include(Faux::IX)
+end
