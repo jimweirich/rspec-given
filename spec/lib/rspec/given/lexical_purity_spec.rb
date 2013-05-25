99446a5c7a3678b9026984cbc66459fd765786c5
Jim Weirich <jim.weirich@gmail.com>
2013-05-25 13:33:52 -0400

    Add lexical purity test

---
+++ spec/lib/rspec/given/lexical_purity_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+LEXICAL_PURITY_GLOBAL_CONSTANT = 3
+
+describe "Lexical Purity" do
+  use_natural_assertions
+  A = 1
+  Given(:avalue) { 1 }
+  context "nested" do
+    B = 2
+    Given(:bvalue) { 2 }
+    Then { A == 1 }
+    Then { avalue == 1 }
+    Then { B == 2 }
+    Then { bvalue == 2 }
+    Then { LEXICAL_PURITY_GLOBAL_CONSTANT == 3 }
+  end
+end
