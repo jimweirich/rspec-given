9f707849be5434c838441eaa930e3f128c10b3bb
Jim Weirich <jim.weirich@gmail.com>
2013-07-08 15:13:58 -0400

    Bump to version 2.4.4

--- lib/rspec/given/version.rb
+++ lib/rspec/given/version.rb
@@ -3,7 +3,7 @@ module RSpec
     VERSION_NUMBERS = [
       VERSION_MAJOR = 2,
       VERSION_MINOR = 4,
-      VERSION_BUILD = 3,
+      VERSION_BUILD = 4,
     ]
     VERSION = VERSION_NUMBERS.join(".")
   end
