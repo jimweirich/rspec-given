799d32f774629811afe4158d2ea1f905a37fb5fa
Jim Weirich <jim.weirich@gmail.com>
2013-07-30 17:16:06 -0400

    Add Precondition/Postcondition/Assert methods for non-test code.

---
+++ lib/given/assertions.rb
@@ -0,0 +1,67 @@
+
+module Given
+  module Assertions
+
+    def self.enable_asserts(enabled=true)
+      @enable_asserts = enabled
+    end
+
+    def self.enable_preconditions(enabled=true)
+      @enable_preconditions = enabled
+    end
+
+    def self.enable_postconditions(enabled=true)
+      @enable_postconditions = enabled
+    end
+
+    def self.asserts?
+      @enable_asserts
+    end
+
+    def self.preconditions?
+      @enable_preconditions
+    end
+
+    def self.postconditions?
+      @enable_postconditions
+    end
+
+    def self.enable_all(enabled=false)
+      enable_asserts enabled
+      enable_preconditions enabled
+      enable_postconditions enabled
+    end
+
+    AssertError = Class.new(StandardError)
+    PreconditionError = Class.new(AssertError)
+    PostconditionError = Class.new(AssertError)
+
+    def Assert(&block)
+      return nil unless Given::Assertions.asserts?
+      unless block.call
+        na = Given::NaturalAssertion.new("Assert", block, self, Given::LineExtractor.new)
+        raise AssertError, na.message
+      end
+    end
+
+    def Precondition(&block)
+      return nil unless Given::Assertions.preconditions?
+      if block.call
+        true
+      else
+        na = Given::NaturalAssertion.new("Precondition", block, self, Given::LineExtractor.new)
+        raise PreconditionError, na.message
+      end
+    end
+
+    def Postcondition(&block)
+      return nil unless Given::Assertions.preconditions?
+      if block.call
+        true
+      else
+        na = Given::NaturalAssertion.new("Postcondition", block, self, Given::LineExtractor.new)
+        raise PostconditionError, na.message
+      end
+    end
+  end
+end
---
+++ spec/lib/given/assertions_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec/given'
+require 'given/assertions'
+
+describe Given::Assertions do
+  Given { extend Given::Assertions }
+
+  describe "Assert { }" do
+    Given { Given::Assertions.enable_asserts }
+
+    context "with true assertion" do
+      When(:result) { Assert { true } }
+      Then { result != Failure() }
+    end
+
+    context "with false assertion" do
+      When(:result) {
+        Assert { false }
+      }
+      Then { result == Failure(Given::Assertions::AssertError, /Assert.*false/) }
+    end
+
+    context "when disabled" do
+      Given { Given::Assertions.enable_asserts false }
+      When(:result) { Assert { false } }
+      Then { result != Failure() }
+    end
+  end
+
+  describe "Precondition { }" do
+    Given { Given::Assertions.enable_preconditions }
+
+    context "with true assertion" do
+      When(:result) { Precondition { true } }
+      Then { result != Failure() }
+    end
+
+    context "with false assertion" do
+      When(:result) {
+        Precondition { false }
+      }
+      Then { result == Failure(Given::Assertions::PreconditionError, /Precondition.*false/) }
+    end
+
+    context "when disabled" do
+      Given { Given::Assertions.enable_preconditions false }
+      When(:result) { Precondition { false } }
+      Then { result != Failure() }
+    end
+  end
+
+  describe "Postcondition { }" do
+    Given { Given::Assertions.enable_preconditions }
+
+    context "with true assertion" do
+      When(:result) { Postcondition { true } }
+      Then { result != Failure() }
+    end
+
+    context "with false assertion" do
+      When(:result) {
+        Postcondition { false }
+      }
+      Then { result == Failure(Given::Assertions::PostconditionError, /Postcondition.*false/) }
+    end
+
+    context "when disabled" do
+      Given { Given::Assertions.enable_preconditions false }
+      When(:result) { Postcondition { false } }
+      Then { result != Failure() }
+    end
+  end
+end
