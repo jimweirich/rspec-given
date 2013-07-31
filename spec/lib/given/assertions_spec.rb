20cc8e156706255dea877b74ec7197031d6e1880
Jim Weirich <jim.weirich@gmail.com>
2013-07-30 22:49:33 -0400

    Disable some more natural assertion tests for unsupported platforms.

--- spec/lib/given/assertions_spec.rb
+++ spec/lib/given/assertions_spec.rb
@@ -1,7 +1,10 @@
+require 'spec_helper'
 require 'rspec/given'
 require 'given/assertions'
 
 describe Given::Assertions do
+  use_natural_assertions_if_supported
+
   Given { extend Given::Assertions }
 
   describe "Assert { }" do
