e9215b018e7e410923284b69318d7a60b11953bd
Jim Weirich <jim.weirich@gmail.com>
2013-07-30 22:37:08 -0400

    Disable use_assertions example for unsupported platforms.

--- examples/use_assertions.rb
+++ examples/use_assertions.rb
@@ -1,42 +1,47 @@
-require 'given/assertions'
-require 'given/fuzzy_number'
+require 'given/module_methods'
 
-include Given::Assertions
-include Given::Fuzzy
+if Given::NATURAL_ASSERTIONS_SUPPORTED
 
-def sqrt(n)
-  Precondition { n >= 0 }
-  result = Math.sqrt(n)
-  Postcondition { result ** 2 == about(n) }
-  result
-end
+  require 'given/assertions'
+  require 'given/fuzzy_number'
 
-def sqrt_bad_postcondition(n)
-  Precondition { n >= 0 }
-  result = Math.sqrt(n)
-  Postcondition { result ** 2 == about(n+1) }
-  result
-end
+  include Given::Assertions
+  include Given::Fuzzy
 
-def use_assert(n)
-  Assert { n == 1 }
-end
-
-def should_fail
-  begin
-    yield
-    fail "Expected error"
-  rescue Given::Assertions::AssertError => ex
-    true
+  def sqrt(n)
+    Precondition { n >= 0 }
+    result = Math.sqrt(n)
+    Postcondition { result ** 2 == about(n) }
+    result
   end
+
+  def sqrt_bad_postcondition(n)
+    Precondition { n >= 0 }
+    result = Math.sqrt(n)
+    Postcondition { result ** 2 == about(n+1) }
+    result
+  end
+
+  def use_assert(n)
+    Assert { n == 1 }
+  end
+
+  def should_fail
+    begin
+      yield
+      fail "Expected error"
+    rescue Given::Assertions::AssertError => ex
+      true
+    end
+  end
+
+  sqrt(1)
+  sqrt(2)
+  sqrt(0)
+
+  should_fail { sqrt(-1) }
+  should_fail { sqrt_bad_postcondition(1) }
+
+  use_assert(1)
+  should_fail { use_assert(0) }
 end
-
-sqrt(1)
-sqrt(2)
-sqrt(0)
-
-should_fail { sqrt(-1) }
-should_fail { sqrt_bad_postcondition(1) }
-
-use_assert(1)
-should_fail { use_assert(0) }
