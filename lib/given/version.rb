25522232befc6ee5849b3377746a6acb4d5492c5
Jim Weirich <jim.weirich@gmail.com>
2013-12-14 16:34:00 -0500

    Bump to version 3.3.0.beta1

--- lib/given/version.rb
+++ lib/given/version.rb
@@ -2,8 +2,9 @@
 module Given
   VERSION_NUMBERS = [
     VERSION_MAJOR = 3,
-    VERSION_MINOR = 2,
+    VERSION_MINOR = 3,
     VERSION_BUILD = 0,
+    BETA = "beta1"
   ]
   VERSION = VERSION_NUMBERS.join(".")
 end
