fb52c43e028a963bec89c4995522b866a9c67dc6
Mark Johnson <markj9@gmail.com>
2015-06-28 22:08:52 -0400

    Change the way minitest-given is loaded for AR models and non-AR models so it works for both at the same time.

--- lib/given/minitest/configure.rb
+++ lib/given/minitest/configure.rb
@@ -1,6 +1,10 @@
 
 # Configure Minitest to use the Given extensions.
-
+if defined?(ActiveSupport::TestCase)
+  ActiveSupport::TestCase.send(:extend, Given::ClassExtensions)
+  ActiveSupport::TestCase.send(:include, Given::FailureMethod)
+  ActiveSupport::TestCase.send(:include, Given::InstanceExtensions)
+end
 Minitest::Spec.send(:extend,  Given::ClassExtensions)
 Minitest::Spec.send(:include, Given::FailureMethod)
 Minitest::Spec.send(:include, Given::InstanceExtensions)
--- lib/minitest/given.rb
+++ lib/minitest/given.rb
@@ -1,15 +1,2 @@
 require 'minitest/spec'
-
-if defined?(ActiveSupport::TestCase)
-  Minitest::Spec.tap do |original_spec|
-    Minitest.send(:remove_const, :Spec)
-    Minitest::Spec = ActiveSupport::TestCase
-
-    require 'given/minitest/all'
-
-    Minitest.send(:remove_const, :Spec)
-    Minitest::Spec = original_spec
-  end
-else
-  require 'given/minitest/all'
-end
+require 'given/minitest/all'
