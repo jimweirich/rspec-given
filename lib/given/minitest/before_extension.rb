470eda100b8486ccd1d216142152cb6f659a5be1
Mark Johnson <markj9@gmail.com>
2015-06-29 22:51:03 -0400

    Modify before extensions to work for TestCase.

--- lib/given/minitest/before_extension.rb
+++ lib/given/minitest/before_extension.rb
@@ -22,15 +22,28 @@ module Minitest
         end
       end
     end
+  end
+end
 
-    # Lazy accessor for Given's before blocks
-    def self._Gvn_before_blocks
-      @_Gvn_before_blocks ||= []
+module ActiveSupport
+  class TestCase
+
+    # Redefine setup to trigger before chains
+    alias original_setup_without_given setup
+    def setup
+      original_setup_without_given
+      _gvn_establish_befores
     end
 
-    # Define a Given style before block
-    def self._Gvn_before(&block)
-      _Gvn_before_blocks << block
+    # Establish the before blocks
+    def _gvn_establish_befores
+      return if defined?(@_gvn_ran_befores) && @_gvn_ran_befores
+      @_gvn_ran_befores = true
+      _gvn_contexts.each do |context|
+        context._Gvn_before_blocks.each do |before_block|
+          instance_eval(&before_block)
+        end
+      end
     end
   end
 end
