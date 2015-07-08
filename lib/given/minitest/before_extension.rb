9e7dd2dcd20c17db0c9223af4f916e70ef649e21
Justin Searls <searls@gmail.com>
2015-07-08 10:27:28 -0400

    Refactor for consistency & remove duplication

--- lib/given/minitest/before_extension.rb
+++ lib/given/minitest/before_extension.rb
@@ -15,44 +15,8 @@ module Given
         _Gvn_before_blocks << block
       end
     end
-  end
-end
 
-module Minitest
-  class Spec
-
-    # Redefine setup to trigger before chains
-    alias original_setup_without_given setup
-    def setup
-      original_setup_without_given
-      _gvn_establish_befores
-    end
-
-    # Establish the before blocks
-    def _gvn_establish_befores
-      return if defined?(@_gvn_ran_befores) && @_gvn_ran_befores
-      @_gvn_ran_befores = true
-      _gvn_contexts.each do |context|
-        context._Gvn_before_blocks.each do |before_block|
-          instance_eval(&before_block)
-        end
-      end
-    end
-  end
-end
-
-if defined?(ActiveSupport::TestCase)
-  module ActiveSupport
-    class TestCase
-
-      # Redefine setup to trigger before chains
-      alias original_setup_without_given setup
-
-      def setup
-        original_setup_without_given
-        _gvn_establish_befores
-      end
-
+    module InstanceExtensions
       # Establish the before blocks
       def _gvn_establish_befores
         return if defined?(@_gvn_ran_befores) && @_gvn_ran_befores
@@ -62,6 +26,30 @@ if defined?(ActiveSupport::TestCase)
             instance_eval(&before_block)
           end
         end
+      end
+    end
+  end
+end
+
+module Minitest
+  class Spec
+    # Redefine setup to trigger before chains
+    alias original_setup_without_given setup
+    def setup
+      original_setup_without_given
+      _gvn_establish_befores
+    end
+  end
+end
+
+if defined?(ActiveSupport::TestCase)
+  module ActiveSupport
+    class TestCase
+      # Redefine setup to trigger before chains
+      alias original_setup_without_given setup
+      def setup
+        original_setup_without_given
+        _gvn_establish_befores
       end
     end
   end
--- lib/given/minitest/configure.rb
+++ lib/given/minitest/configure.rb
@@ -4,10 +4,12 @@ if defined?(ActiveSupport::TestCase)
   ActiveSupport::TestCase.send(:extend, Given::MiniTest::ClassExtensions)
   ActiveSupport::TestCase.send(:include, Given::FailureMethod)
   ActiveSupport::TestCase.send(:include, Given::InstanceExtensions)
+  ActiveSupport::TestCase.send(:include, Given::MiniTest::InstanceExtensions)
 end
 # Configure Minitest to use the Given extensions.
 Minitest::Spec.send(:extend,  Given::ClassExtensions)
 Minitest::Spec.send(:extend, Given::MiniTest::ClassExtensions)
 Minitest::Spec.send(:include, Given::FailureMethod)
 Minitest::Spec.send(:include, Given::InstanceExtensions)
+Minitest::Spec.send(:include, Given::MiniTest::InstanceExtensions)
 Given.use_natural_assertions if Given::NATURAL_ASSERTIONS_SUPPORTED
