a77e8edbfad898cd0792ba731b415b2287baf561
Justin Searls <searls@gmail.com>
2016-01-13 09:11:25 -0500

    It's sufficient to only configure spec_helper here
    Adding the configuration here isn't necessary so long as the spec_helper
    is set up correctly, even if you only run the example spec directly.

--- examples/example_helper.rb
+++ examples/example_helper.rb
@@ -3,15 +3,6 @@ $LOAD_PATH << './examples/stack'
 if defined?(RSpec)
   require 'rspec/given'
   require 'spec_helper'
-
-  RSpec.configure do |config|
-    # Before RSpec 3.4, RSpec would only print lines in failures from spec files.
-    # Starting in 3.4, it now prints lines from the new `project_source_dirs` config
-    # setting. We want it to look for lines from our examples, and since its not the
-    # standard `spec` dir, we have to tell RSpec about it here.
-    # See #18 for more discussion.
-    config.project_source_dirs << "examples" if config.respond_to?(:project_source_dirs)
-  end
 else
   require 'minitest/autorun'
   require 'active_support_helper'
