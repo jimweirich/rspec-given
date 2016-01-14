9ff92e86d43e85757f515c4eb2f963cdc0e0ce9d
Justin Searls <searls@gmail.com>
2016-01-14 14:23:42 -0500

    3.8.0

--- lib/given/version.rb
+++ lib/given/version.rb
@@ -2,8 +2,8 @@
 module Given
   VERSION_NUMBERS = [
     VERSION_MAJOR = 3,
-    VERSION_MINOR = 7,
-    VERSION_BUILD = 1,
+    VERSION_MINOR = 8,
+    VERSION_BUILD = 0,
   ]
   VERSION = VERSION_NUMBERS.join(".")
 end
